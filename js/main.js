8d6ceb006ced4e6385a36e6529c3dc134c7baa88
Andrew Bao <andrewbao@Andrews-MacBook-Pro-3.local>
2018-04-26 19:10:08 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,13 +1,36 @@
-console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];  
-var cardOne = cards[0]; 
-cardsInPlay.push(cardOne); 
-console.log("User flipped queen"); 
-var cardTwo = cards[2]; 
-cardsInPlay.push(cardTwo); 
-console.log("User flipped king"); 
+
+
+var checkForMatch = function(){ 
+if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+}
+} 
+
+
+var flipCard = function(cardId){ 
+
+var cardOne = cards[0];
+cardsInPlay.push(cards[0]);
+console.log("User flipped " + cards[0]); 
+
+var cardTwo = cards[2];
+cardsInPlay.push(cards[2]);
+console.log("User flipped " + cards[2]);  
+
+
 if (cardsInPlay.length === 2){alert("Sorry, try again!")} 
-	else if (cardsInPlay[0] === cardsInPlay[1]) {alert("You found a match!");}
+	else if (cardsInPlay[0] === cardsInPlay[1]) {alert("You found a match!");}   
+
+checkForMatch;
+
+};  
+
+flipCard (0); 
+flipCard (2);
+
 
 
